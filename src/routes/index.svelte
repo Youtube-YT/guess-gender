784e35a0c56c2bd552bcde692dfc9891b1ace93e
Youtube-YT <mingweiop@gmail.com>
2021-12-19 03:47:18 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let gender = "";
+    let name = "";
+    async function getGender() {
+      fetch('https://api.genderize.io/?name=' + name)
+        .then((response) => response.json())
+        .then((data) => {
+          //Able to change whatever name you like Example: weatherTemparature = data.temperature the name of data should not be changed.
+          name = data.name; // Path for name
+          gender = data.gender; // Path for gender
+        });
+    }
+        </script>
+        <h1>Guess Your Gender 🤔</h1>
+
+        <form on:submit|preventDefault={getGender}>    
+            <label for="location">Name:</label>
+            <input id="location" bind:value={name} type="text" />
+            <button type="submit">Check</button>
+        </form>
+        <p>Gender: {gender}</p>
